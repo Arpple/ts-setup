446f38b13c7f869ffe3ccc89487e318a1d79ebcb
Arpple <apple0239@gmail.com>
2018-10-02 19:59:57 +0700

    add compiler option lib es6

--- src/fn.test.ts
+++ src/fn.test.ts
@@ -10,4 +10,8 @@ describe("fn", () => {
     it("support es6 import", () => {
         assert.isNotNull(greeter)
     })
+
+    it("support es6 Array.find()", () => {
+        assert.equal([1, 2, 3, 4, 5].find(x => x / 2 === 1), 2)
+    })
 })
