5017441610b052e86490e3ebeae102e42c70aee0
Arpple <apple0239@gmail.com>
2018-11-15 01:18:22 +0700

    remove ts-sinon

--- src/example/service.test.ts
+++ src/example/service.test.ts
@@ -1,18 +1,18 @@
 import { assert } from "chai"
-import { stubInterface } from "ts-sinon"
-import * as sinon from "sinon"
 import { SomeService, doSomething, SomeAsyncService, doSomethingAsync } from "./service"
 
 describe("example/service", () => {
     it("test mocking service", () => {
-        const service = stubInterface<SomeService>()
-        service.doSomething = sinon.stub().returns(true)
+        const service: SomeService = {
+            doSomething: () => true
+        }
         assert.isTrue(doSomething(service))
     })
 
     it("test mocking async service", (done) => {
-        const service = stubInterface<SomeAsyncService>()
-        service.doSomething = sinon.stub().returns(Promise.resolve(true))
+        const service: SomeAsyncService = {
+            doSomething: () => Promise.resolve(true)
+        }
         doSomethingAsync(service)
             .then((result) => {
                 assert.isTrue(result)
