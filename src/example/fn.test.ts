afc73eae1cb69f00b0add9475f06c9a97ba1d8c4
Arpple <apple0239@gmail.com>
2018-12-27 16:40:56 +0700

    remove unused dep, sinon

--- src/example/fn.test.ts
+++ src/example/fn.test.ts
@@ -1,5 +1,4 @@
 import test from "ava"
-import * as sinon from "sinon"
 import greeter from "./greeter"
 
 test("example/fn", (assert) => {
@@ -8,7 +7,4 @@ test("example/fn", (assert) => {
 
     assert.truthy(greeter, "support import statement")
     assert.is([1, 2, 3, 4, 5].find((x) => x / 2 === 1), 2, "support es6 Array.find()")
-
-    const fake = sinon.fake.returns(4)
-    assert.is(fake(), 4, "have sinon.fake")
 })
